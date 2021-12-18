ab30ae0ebef66309e4de0b189e601a7fe611f948
Rendy Kuo <maxgreg13625@gmail.com>
2021-12-18 20:26:37 +0800

    keep fastest 13

--- 13_roman_to_integer/py/main.py
+++ 13_roman_to_integer/py/main.py
@@ -11,32 +11,32 @@ class Solution(object):
 			'C': 100, 'D': 500,
 			'M': 1000}
 
-		# implement by while loop  40ms/13.4MB
-		idx = 0
-		while idx < len(s):
-			if idx < len(s) - 1 and\
-				roman_dict[s[idx]] < roman_dict[s[idx+1]]:
-				result = result + roman_dict[s[idx+1]] - roman_dict[s[idx]]
-				idx = idx + 1
-			else:
-				result = result + roman_dict[s[idx]]
-			idx = idx + 1
-		return result
-		## implement by for loop   36ms/13.5MB
-		#is_skip = False		
-		#for idx, val in enumerate(s):
-		#	if not is_skip:
-		#		# if next digit larger than current digit
-		#		# then add next digit and minus current digit
-		#		if idx < len(s) - 1 and\
-		#			roman_dict[s[idx]] < roman_dict[s[idx+1]]:
-		#			result = result + roman_dict[s[idx+1]] - roman_dict[s[idx]]
-		#			is_skip = True
-		#		else:
-		#			result = result + roman_dict[s[idx]]
+		## implement by while loop  40ms/13.4MB
+		#idx = 0
+		#while idx < len(s):
+		#	if idx < len(s) - 1 and\
+		#		roman_dict[s[idx]] < roman_dict[s[idx+1]]:
+		#		result = result + roman_dict[s[idx+1]] - roman_dict[s[idx]]
+		#		idx = idx + 1
 		#	else:
-		#		is_skip = False
+		#		result = result + roman_dict[s[idx]]
+		#	idx = idx + 1
 		#return result
+		# implement by for loop   36ms/13.5MB
+		is_skip = False		
+		for idx, val in enumerate(s):
+			if not is_skip:
+				# if next digit larger than current digit
+				# then add next digit and minus current digit
+				if idx < len(s) - 1 and\
+					roman_dict[s[idx]] < roman_dict[s[idx+1]]:
+					result = result + roman_dict[s[idx+1]] - roman_dict[s[idx]]
+					is_skip = True
+				else:
+					result = result + roman_dict[s[idx]]
+			else:
+				is_skip = False
+		return result
 
 assert Solution().romanToInt('III') == 3
 assert Solution().romanToInt('LVIII') == 58
