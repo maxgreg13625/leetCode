24583c08b45b2e7790fef7fa87b3764f7ca2c9b9
Rendy <maxgreg13625@gmail.com>
2021-12-25 15:19:22 +0800

    recursive for 21

--- 21_merge_two_sorted_lists/py/main.py
+++ 21_merge_two_sorted_lists/py/main.py
@@ -4,16 +4,28 @@ class ListNode(object):
         self.next = next
 
 class Solution(object):
-    def mergeTwoLists(self, list1, list2):
+    def mergeTwoLists(self, l1, l2):
         """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        list1.extend(list2)
-        return sorted(list1)
-
-
-assert Solution().mergeTwoLists([1, 2, 4], [1, 3, 4]) == [1,1,2,3,4,4]
-assert Solution().mergeTwoLists([], []) == []
-assert Solution().mergeTwoLists([], [0]) == [0]
+        ## 40ms/14.2MB
+        #if l1 and l2:
+        #    if l1.val > l2.val:
+        #        l1, l2 = l2, l1
+        #    l1.next = self.mergeTwoLists(l1.next, l2)
+        #    return l1
+        #else:
+        #    return l1 or l2
+        ## 40ms/14.3MB
+        #if l1 is None:
+        #    return l2
+        #elif l2 is None:
+        #    return l1
+        #if l1.val <= l2.val:
+        #    l1.next = self.mergeTwoLists(l1.next, l2)
+        #    return l1
+        #else:
+        #    l2.next = self.mergeTwoLists(l1, l2.next)
+        #    return l2
