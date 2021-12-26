59fb0982cc75e83bd4a798ed5120ecbf5f19a2ab
Rendy <maxgreg13625@gmail.com>
2021-12-26 09:27:24 +0800

    add loop method for 35

--- 35_search_insert_position/py/main.py
+++ 35_search_insert_position/py/main.py
@@ -5,7 +5,7 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        # 32ms/14.2MB
+        # recursive: 32ms/14.2MB
         length = len(nums)
         if target > nums[-1]:
             return length
@@ -17,6 +17,18 @@ class Solution(object):
                 return mid + self.searchInsert(nums[mid:], target)
             else:
                 return self.searchInsert(nums[:mid], target)
+        ## loop solution  36ms/14.3MB
+        #i = 0
+        #j = len(nums) - 1
+        #while(i <= j):
+        #    pivot = (i + j) // 2
+        #    if (nums[pivot] == target):
+        #        return pivot
+        #    elif (nums[pivot] >= target):
+        #        j = pivot - 1
+        #    else:
+        #        i = pivot + 1
+        #return i
 
 assert Solution().searchInsert([1, 3, 5, 6], 5) == 2
 assert Solution().searchInsert([1, 3, 5, 6], 2) == 1
