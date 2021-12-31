a5241ed737f59b74448310586eabec1a8eb51c29
Rendy <maxgreg13625@gmail.com>
2021-12-31 15:59:13 +0800

    improve performance for add two numbers

--- 2_add_two_numbers/py/main.py
+++ 2_add_two_numbers/py/main.py
@@ -14,7 +14,42 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        # 77ms/13.6MB
+        ## 86ms/13.6MB
+        #head_result = temp_result = ListNode()
+        #while l1 or l2:
+        #    if l1:
+        #        temp_result.val = temp_result.val + l1.val
+        #        l1 = l1.next
+        #    if l2:
+        #        temp_result.val = temp_result.val + l2.val
+        #        l2 = l2.next
+        #    if l1 or l2 or temp_result.val >= 10:
+        #        temp_result.next = ListNode()
+        #        if temp_result.val >= 10:
+        #            temp_result.next.val += 1
+        #            temp_result.val = temp_result.val % 10
+        #        temp_result = temp_result.next
+        #return head_result
+        ## 77ms/13.6MB
+        #head_result = temp_result = ListNode()
+        #while l1 or l2:
+        #    if l1 and l2:
+        #        temp_result.val = temp_result.val + l1.val + l2.val
+        #        l1, l2 = l1.next, l2.next
+        #    elif l1:
+        #        temp_result.val = temp_result.val + l1.val
+        #        l1 = l1.next
+        #    else:
+        #        temp_result.val = temp_result.val + l2.val
+        #        l2 = l2.next
+        #    if l1 or l2 or temp_result.val >= 10:
+        #        temp_result.next = ListNode()
+        #        if temp_result.val >= 10:
+        #            temp_result.next.val += 1
+        #            temp_result.val = temp_result.val % 10
+        #        temp_result = temp_result.next
+        #return head_result
+        # 52ms/13.5MB
         head_result = temp_result = ListNode()
         while l1 or l2:
             if l1 and l2:
@@ -28,8 +63,6 @@ class Solution(object):
                 l2 = l2.next
             if l1 or l2 or temp_result.val >= 10:
                 temp_result.next = ListNode()
-                if temp_result.val >= 10:
-                    temp_result.next.val += 1
-                    temp_result.val = temp_result.val % 10
+                temp_result.next.val, temp_result.val = divmod(temp_result.val, 10)
                 temp_result = temp_result.next
         return head_result
