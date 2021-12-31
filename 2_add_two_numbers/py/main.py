e4e53755ee9408db1847107f80655f4bd70a8a10
Rendy <maxgreg13625@gmail.com>
2021-12-31 14:33:26 +0800

    1st add for add two numbers

---
+++ 2_add_two_numbers/py/main.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+from typing import List
+
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        # 77ms/13.6MB
+        head_result = temp_result = ListNode()
+        while l1 or l2:
+            if l1 and l2:
+                temp_result.val = temp_result.val + l1.val + l2.val
+                l1, l2 = l1.next, l2.next
+            elif l1:
+                temp_result.val = temp_result.val + l1.val
+                l1 = l1.next
+            else:
+                temp_result.val = temp_result.val + l2.val
+                l2 = l2.next
+            if l1 or l2 or temp_result.val >= 10:
+                temp_result.next = ListNode()
+                if temp_result.val >= 10:
+                    temp_result.next.val += 1
+                    temp_result.val = temp_result.val % 10
+                temp_result = temp_result.next
+        return head_result
